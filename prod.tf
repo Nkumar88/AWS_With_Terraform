988e3b4c224006b778a1db2ac1aca36bafd04495
Nitesh <nit.glad@gmail.com>
2021-05-09 15:04:31 +0200

    adapt default vpc and rename tf file to prod

--- prod.tf
+++ prod.tf
@@ -3,7 +3,9 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-resource "aws_s3_bucket" "tf_learning"{
+resource "aws_s3_bucket" "prod_tf_learning"{
   bucket = "tf-learning-nit-202105091403"
   acl = "private"
 }
+
+resource "aws_default_vpc" "default" {}
